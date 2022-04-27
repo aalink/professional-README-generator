b1e7f59e998d770a13a26406ccdf5b117299b1f3
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-04-27 18:51:06 -0400

    Changing structure of questions

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
+const filename = `Finished-README.md`;
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [
-  "What is the title of the project?",
-  "Give a description of the project.",
-  "Give installation instructions",
-  "What is the usage of this project?",
-  "What kind of license does it have?",
-  "Let other developers know how they can contribute.",
-  "How will you test the application?",
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is the title of the project?",
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "Give a description of the project.",
+        },
+        {
+          type: "input",
+          name: "installation",
+          message: "Give installation instructions",
+        },
+        {
+          type: "input",
+          name: "usage",
+          message: "What is the usage of this project?",
+        },
+        {
+          type: "input",
+          name: "license",
+          message: "What kind of license does it have?",
+        },
+        {
+          type: "input",
+          name: "contribute",
+          message: "Let other developers know how they can contribute.",
+        },
+        {
+          type: "input",
+          name: "testing",
+          message: "How will you test the application?",
+        },
+      ]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(nameOfFile, data) {
+}
 
+
+// #######################################################################################
+// #######################################################################################
+// #######################################################################################
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: questions[0],
-    },
-    {
-      type: "input",
-      name: "description",
-      message: questions[1],
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: questions[2],
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: questions[3],
-    },
-    {
-      type: "input",
-      name: "license",
-      message: questions[4],
-    },
-    {
-      type: "input",
-      name: "contribute",
-      message: questions[5],
-    },
-    {
-      type: "input",
-      name: "testing",
-      message: questions[6],
-    },
-  ]);
+    questions()
 }
 
 // Function call to initialize app
