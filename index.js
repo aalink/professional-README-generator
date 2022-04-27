abca6add8ea910d1d98b4bac2bb64a86483ee2e6
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-04-27 15:35:38 -0400

    Adding questions to 'const questions' array

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of the project?', 'Give a description of the project.', 'What does the Table of Contents contain?', 'Give installation instructions', 'What is the usage of this project?', 'What kind of license does it have?', 'Let other developers know how they can contribute.', 'How will you test the application?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
+
