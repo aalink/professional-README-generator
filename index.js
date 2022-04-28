25547e6025e08e7ccb037feeb8c4e47039af4f47
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-04-27 21:01:50 -0400

    Adding github and email prompts

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
-const filename = `Finished-README.md`;
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = () => {
-    return inquirer.prompt([
-        {
-          type: "input",
-          name: "title",
-          message: "What is the title of the project?",
-        },
-        {
-          type: "input",
-          name: "description",
-          message: "Give a description of the project.",
-        },
-        {
-          type: "input",
-          name: "installation",
-          message: "Give installation instructions",
-        },
-        {
-          type: "input",
-          name: "usage",
-          message: "What is the usage of this project?",
-        },
-        {
-          type: "input",
-          name: "license",
-          message: "What kind of license does it have?",
-        },
-        {
-          type: "input",
-          name: "contribute",
-          message: "Let other developers know how they can contribute.",
-        },
-        {
-          type: "input",
-          name: "testing",
-          message: "How will you test the application?",
-        },
-      ]);
-}
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of the project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Give a description of the project.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Give installation instructions",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the usage of this project?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "What kind of license does it have?",
+    },
+    {
+      type: "input",
+      name: "contribute",
+      message: "Let other developers know how they can contribute.",
+    },
+    {
+      type: "input",
+      name: "testing",
+      message: "How will you test the application?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "At what email address should people reach you?",
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(nameOfFile, data) {
-}
 
+}
 
 // #######################################################################################
 // #######################################################################################
 // #######################################################################################
 // TODO: Create a function to initialize app
 function init() {
-    questions()
+  questions();
 }
 
 // Function call to initialize app
