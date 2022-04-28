b9a3e75664ac333697736ee6352fcda877eca6e2
Aaron Yacher <29718914+aalink@users.noreply.github.com>
2022-04-28 12:59:27 -0400

    Adds to renderLicenseBadge & renderLicenseSection Also adding functions to the module.exports variable

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})
+  `
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,7 +11,15 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return ``
+  }
+  return `
+  ## License
+> ${renderLicenseBadge(license)}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -17,4 +28,4 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown, renderLicenseBadge, renderLicenseLink, renderLicenseSection};
